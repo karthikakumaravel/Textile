c34d5efcaa4dcf22f693bdd235b0d71813eff819
Jayavarshini-28 <126934645+Jayavarshini-28@users.noreply.github.com>
2024-05-15 00:41:34 +0530

    Update CreateProduct.js

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -21,7 +21,7 @@ const CreateProduct = () => {
   //get all category
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("http://localhost:8080/api/v1/category/get-category");
+      const { data } = await axios.get("http://localhost:10000/api/v1/category/get-category");
       if (data?.success) {
         setCategories(data?.category);
       }
