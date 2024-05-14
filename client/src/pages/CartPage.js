49769cf1fe612772049328cdb7cf0235b88d5b87
Jayavarshini-28 <126934645+Jayavarshini-28@users.noreply.github.com>
2024-05-15 00:48:00 +0530

    Update CartPage.js

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -51,7 +51,7 @@ const CartPage = () => {
     try {
       setLoading(true);
       const { nonce } = await instance.requestPaymentMethod();
-      const { data } = await axios.post("http://localhost:8080/api/v1/product/braintree/payment", {
+      const { data } = await axios.post("http://localhost:10000/api/v1/product/braintree/payment", {
         nonce,
         cart,
       });
@@ -91,7 +91,7 @@ const CartPage = () => {
                 <div className="row card flex-row" key={p._id}>
                   <div className="col-md-4">
                     <img
-                      src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
+                      src={`http://localhost:10000/api/v1/product/product-photo/${p._id}`}
                       className="card-img-top"
                       alt={p.name}
                       width="100%"
